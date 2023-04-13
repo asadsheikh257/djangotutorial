62abf98ffac7424ddfe06720a19571c8e11aaae5
Asad <asadsheikh257@gmail.com>
2023-04-13 22:36:43 +0500

    changes little bit space formating

--- home/views.py
+++ home/views.py
@@ -12,12 +12,12 @@ def home(request):
         {'Name':'Mohsin', 'Age':17},
         {'Name':'Waqas', 'Age':16}
     ]
-    
     return render(request, "home/index.html", context={'peoples': peoples, 'page':'Django Tutorial'})
 
 def about(request):
     context = {'page':'about'}
     return render(request, "home/about.html", context) 
+
 def contact(request):
     context = {'page':'contact'}
     return render(request, "home/contact.html", context)
