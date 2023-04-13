b00120253cc1f795e0187f66ee7a7d741ea37417
Asad <asadsheikh257@gmail.com>
2023-04-13 22:32:24 +0500

    concept of views, url and breif things about template engine

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
 urlpatterns = [
+    path('', home, name="home"),
+
+    path('about/', about, name = "about"),
+    path('contact/', contact, name = "contact"),
+
+
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    peoples = [
+        {'Name':'Asad', 'Age':23},
+        {'Name':'Ali', 'Age':18},
+        {'Name':'Adnan', 'Age':24},
+        {'Name':'Mohsin', 'Age':17},
+        {'Name':'Waqas', 'Age':16}
+    ]
+    
+    return render(request, "home/index.html", context={'peoples': peoples, 'page':'Django Tutorial'})
+
+def about(request):
+    context = {'page':'about'}
+    return render(request, "home/about.html", context) 
+def contact(request):
+    context = {'page':'contact'}
+    return render(request, "home/contact.html", context)
